cfa83dc865c968b0f742d7742c5b420be2b12c5f
Daniel Siepmann <coding@daniel-siepmann.de>
2018-11-04 00:34:23 +0100

    TASK: Add acceptance tests with codeception

---
+++ tests/Classes/Acceptance/Typo3Environment.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Codappix\Tests\Acceptance;
+
+/*
+ * Copyright (C) 2018 Daniel Siepmann <coding@daniel-siepmann.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+use Codeception\Event\TestEvent;
+use Codeception\Event\SuiteEvent;
+use Codeception\Events;
+use PHPUnit\Framework\TestSuite;
+use TYPO3\CMS\Core\Utility\ArrayUtility;
+use TYPO3\TestingFramework\Core\Acceptance\Extension\BackendEnvironment;
+
+class Typo3Environment extends BackendEnvironment
+{
+    public function _initialize()
+    {
+        ArrayUtility::mergeRecursiveWithOverrule($this->config, [
+            // 'typo3DatabaseDriver' => 'pdo_sqlite',
+            'typo3DatabaseDriver' => 'pdo_mysql',
+            'typo3DatabaseHost' => 'localhost',
+            'typo3DatabaseUsername' => 'testing',
+            'typo3DatabasePassword' => 'testing',
+            'typo3DatabaseName' => 'workshopgitlabtesting',
+            'typo3DatabasePort' => '3306',
+            'coreExtensionsToLoad' => [
+                'core',
+                'extbase',
+                'fluid',
+                'fluid_styled_content',
+                'frontend',
+                'backend',
+            ],
+            'testExtensionsToLoad' => [
+                'typo3conf/ext/example_extension',
+            ],
+            'xmlDatabaseFixtures' => [
+                dirname(__FILE__) . '/../../acceptance/fixtures/full.xml',
+            ],
+        ]);
+
+        parent::_initialize();
+    }
+}
---
+++ tests/acceptance/AddressCest.php
@@ -0,0 +1,37 @@
+<?php
+
+class AddressCest
+{
+    public function listsExistingRecords(AcceptanceTester $I)
+    {
+        $I->amOnPage('/plugin');
+        $I->see('Codappix GmbH');
+    }
+
+    public function canEditRecord(AcceptanceTester $I)
+    {
+        $I->amOnPage('/plugin');
+        $I->see('Edit');
+        $I->click('Edit');
+
+        $I->see('Editing: Codappix GmbH');
+        $I->fillField(['id' => 'companyName'], 'TYPO3 Camp Rhein Ruhr');
+        $I->click('Update');
+
+        $I->amOnPage('/plugin');
+        $I->see('TYPO3 Camp Rhein Ruhr');
+    }
+
+    public function zipValidatesAgainst5Digits(AcceptanceTester $I)
+    {
+        $I->amOnPage('/plugin');
+        $I->see('Edit');
+        $I->click('Edit');
+
+        $I->see('Editing: TYPO3 Camp Rhein Ruhr');
+        $I->fillField(['id' => 'zip'], '123');
+        $I->click('Update');
+
+        $I->see('Please provide a valid ZIP consisting of 5 digits.');
+    }
+}
